ACCESS-NRI/GFDL-generic-tracers

commit b78f0e00564a2e7baf5608e84fbec2867c813380

WOMBATlite: Only tune dFe to detection limit below 200m

generic_tracers/generic_WOMBATlite.F90
@@ -3149,8 +3149,12 @@ module generic_WOMBATlite
         ! pjb: tune minimum dissolved iron concentration to detection limit...
         !       this is essential for ensuring dFe is replenished in upper ocean and actually
         !       looks to be the secret of PISCES ability to replicate dFe limitation in the right places
-        wombat%f_fe(i,j,k) = max(wombat%dfefloor * umol_m3_to_mol_kg, &
-                                 wombat%f_fe(i,j,k)) * grid_tmask(i,j,k)
+        ! Only do this where the ocean is > 200m deep to avoid truncating negative tracer concentrations
+        ! at river mouths
+        if (wombat%zw(i,j,k) > 200) then
+          wombat%f_fe(i,j,k) = max(wombat%dfefloor * umol_m3_to_mol_kg, &
+                                wombat%f_fe(i,j,k)) * grid_tmask(i,j,k)
+        endif
       enddo
     enddo; enddo
 

commit 02023e4696286711da9fcec4f60267f418fcc0f8

WOMBATlite: Fix iron trunc error introduced in b78f0e00564a2e7baf5608e84fbec2867c813380

generic_tracers/generic_WOMBATlite.F90
@@ -3160,22 +3160,23 @@ module generic_WOMBATlite
     !-----------------------------------------------------------------------!
     !-----------------------------------------------------------------------!
     do j = jsc,jec; do i = isc,iec;
-      ! mac: bottom dFe fix to 1 nM when the water is <= 200 m deep.
       if (grid_kmt(i,j) > 0) then
         k = grid_kmt(i,j)
-        if (wombat%zw(i,j,k) <= 200) wombat%p_fe(i,j,k,tau) = umol_m3_to_mol_kg * 0.999 ! [mol/kg]
-      endif
-      do k = 1,nk
-        ! pjb: tune minimum dissolved iron concentration to detection limit...
-        !       this is essential for ensuring dFe is replenished in upper ocean and actually
-        !       looks to be the secret of PISCES ability to replicate dFe limitation in the right places
-        ! Only do this where the ocean is > 200m deep to avoid truncating negative tracer concentrations
-        ! at river mouths
-        if (wombat%zw(i,j,k) > 200) then
-          wombat%p_fe(i,j,k,tau) = max(wombat%dfefloor * umol_m3_to_mol_kg, &
-                                wombat%p_fe(i,j,k,tau)) * grid_tmask(i,j,k)
+        if (wombat%zw(i,j,k) <= 200) then
+          ! mac: bottom dFe fix to 1 nM when the water is <= 200 m deep.
+          wombat%p_fe(i,j,k,tau) = umol_m3_to_mol_kg * 0.999 ! [mol/kg]
+        else
+          do k = 1,nk
+            ! pjb: tune minimum dissolved iron concentration to detection limit...
+            !       this is essential for ensuring dFe is replenished in upper ocean and actually
+            !       looks to be the secret of PISCES ability to replicate dFe limitation in the right places
+            ! Only do this where the ocean is > 200m deep to avoid truncating negative tracer concentrations
+            ! at river mouths
+            wombat%p_fe(i,j,k,tau) = max(wombat%dfefloor * umol_m3_to_mol_kg, &
+                                        wombat%p_fe(i,j,k,tau)) * grid_tmask(i,j,k)
+          enddo
         endif
-      enddo
+      endif
     enddo; enddo
 
 